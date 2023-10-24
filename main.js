fa302260dfc23608d3ce0920cab7abc96d288afc
MickeMarten <KarlMikael.Martensson@gmail.com>
2023-10-24 13:04:09 +0200

    patch 2.2

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ buttonDiv.appendChild(removeButton);
 buttonDiv.appendChild(aboutMeInfo);
 removeButton.className="removeButton";
 removeButton.innerText="x";
+aboutMeInfo.className="aboutMeInfo";
 aboutMeInfo.innerText = "Hej jag heter Mikael Mårtensson och jag bor i NOrrköping. Jag vill gärna jobba hos er så länge som det går. Jag jobbar jättebra och jag är snäll mot andra kollegor. Alla kollegor är lika värda för mig och jag lägger ingen vikt i att vi är Hej jag heter Mikael Mårtensson och jag bor i NOrrköping. Jag vill gärna jobba hos er så länge som det går. Jag jobbar jättebra och jag är snäll mot andra kollegor. Alla kollegor äHej jag heter Mikael Mårtensson och jag bor i NOrrköping. Jag vill gärna jobba hos er så länge som det går. Jag jobbar jättebra och jag är snäll mot andra kollegor. Alla kollegor äolika";
 
 
